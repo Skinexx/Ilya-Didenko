d33b0edff39da28c4a33c36f357c38d51f7930cb
Skinexx <ilusaidenko28@gmail.com>
2024-03-05 17:43:30 +0200

    PatrolEnemies

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -8,8 +8,20 @@ public class WaypointPatrol : MonoBehaviour
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
 
+    private int currentWaypointIndex;
+
     private void Start()
     {
         navMeshAgent.SetDestination(waypoints[0].position);
     }
+
+    private void Update()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
+    }
+
 }
